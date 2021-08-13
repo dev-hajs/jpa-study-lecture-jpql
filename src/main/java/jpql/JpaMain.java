b6b8fe0498b672cadcf533bd0ecd3b4c8c59d125
dev-hajs <dev.hajs@gmail.com>
2021-08-13 17:02:04 +0900

    페치 조인1 - 기본 -. 엔티티 페치 조인 -. 컬렉션 페치 조인

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -5,10 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-import javax.persistence.OrderColumn;
 import javax.persistence.Persistence;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
 
 public class JpaMain {
 
@@ -22,30 +19,52 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
             Member member1 = new Member();
-            member1.setUsername("관리자1");
-            member1.setTeam(team);
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
-            member2.setTeam(team);
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-//            String query = "select m.username from Member m";   // String.class
-//            String query = "select m.team from Member m";       // Team.class
-            String query = "select t.members from Team t";       // Collection.class
-            List<Collection> resultList = em.createQuery(query, Collection.class)
-                .getResultList();
+            /* 엔티티 페치 조인 */
+//            String query = "SELECT m FROM Member m JOIN FETCH m.team";
+//            List<Member> resultList = em.createQuery(query, Member.class)
+//                .getResultList();
+//            for (Member member : resultList) {
+//                System.out.println("member = " + member.getUsername() + " , " + member.getTeam().getName());
+                // 회원1, 팀A(SQL)
+                // 회원2, 팀A(1차캐시)
+                // 회원3, 팀B(SQL)
+//            }
 
-            for (Object s : resultList) {
-                System.out.println("s = " + s);
+            /* 컬렉션 페치 조인 */
+            String query = "SELECT distinct t FROM Team t JOIN FETCH t.members";
+            List<Team> resultList = em.createQuery(query, Team.class)
+                .getResultList();
+            for (Team team : resultList) {
+                System.out.println("team = " + team.getName() + " | members=" + team.getMembers().size());
+                for (Member member : team.getMembers()) {
+                    System.out.println(" -> member = " + member);
+                }
             }
 
             tx.commit();
